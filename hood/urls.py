ff9ac95b8db6deaefc070054dc4dd7b043eb5244
sylvester omondi <sylvesteromondi49@gmail.com>
2019-01-11 17:29:09 +0300

    url paths

--- hood/urls.py
+++ hood/urls.py
@@ -5,4 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     url(r'^$',views.hood,name='hood'),
+    url(r'^post',views.new_neighbour,name='new_neighbour')
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
