c0066f72e5defe9efa84d3e4b7c92d9954425366
sylvester omondi <sylvesteromondi49@gmail.com>
2019-01-11 17:29:42 +0300

    defined the new path of my neighbour  class

--- hood/views.py
+++ hood/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render,redirect
-
+from .models import *
 from django.http import HttpResponse, Http404,HttpResponseRedirect
-
+from .forms import *
 
 # Create your views here.
 def hood(request):
+    current_user= request.user
+    neighbour=Neighbourhood.objects.all()
+    
 
     return render(request, 'index.html')
+
+def new_neighbour(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NeighbourhoodForm(request.POST, request.FILES)
+        if form.is_valid():
+            neighbour = form.save(commit=False)
+            neighbour.user = current_user
+            neighbour.save()
+        return redirect('hood')
+
+    else:
+        form = NeighbourhoodForm()
+    return render(request,'hood.html', {"form": form})  
+
+
