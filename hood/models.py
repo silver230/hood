a5238cc8d1fd63b0b7883e8707d634e01cae3e3c
sylvester omondi <sylvesteromondi49@gmail.com>
2019-01-11 17:28:51 +0300

    created methods to delete,save annd show all of my fields

--- hood/models.py
+++ hood/models.py
@@ -1,12 +1,30 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
+from pyuploadcare.dj.models import ImageField
 
 # Create your models here.
 class Neighbourhood(models.Model):
     user = models.ForeignKey(User, related_name="poster", on_delete=models.CASCADE)
-    neighbourhood_location = models.CharField(max_length=40)
-    neighbourhood_name = models.CharField(max_length=40)
+    location = models.CharField(max_length=40,blank=True)
+    name = models.CharField(max_length=40,blank=True)
     occupants_count = models.IntegerField(default=0)
     area = ImageField(manual_crop='')
+
+
+    @classmethod
+    def get_all(cls):
+        neighbourhood = Neighbourhood.objects.all()
+        return neighbourhood
+
+    @classmethod
+    def save_neighbourhood(self):
+        return self.save()
+
+    @classmethod
+    def delete_neighbourhood(self):
+        return self.delete()
+
 
     def __str__(self):
         return self.name
