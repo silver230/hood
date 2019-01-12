b88006a82024f6d1652052b8a4f7145f42798b84
sylvester omondi <sylvesteromondi49@gmail.com>
2019-01-12 12:26:34 +0300

    created bussiness class

--- hood/models.py
+++ hood/models.py
@@ -30,4 +30,35 @@ class Neighbourhood(models.Model):
         return self.name
 
 
+class Business(models.Model):
+    user = models.ForeignKey(User, related_name="silver", on_delete=models.CASCADE)
+    neighbourhood = models.ForeignKey(Neighbourhood, related_name="neigh", on_delete=models.CASCADE)
+    business_name = models.CharField(max_length=40)
+    business_emails = models.CharField(max_length=40)
     
+
+    def get_absolute_url(self):
+        return reverse('dump', kwargs={'pk':self.pk})
+
+
+    def __str__(self):
+        return self.business_name
+
+    @classmethod
+    def show_business(cls):
+        business = cls.objects.order_by('business_name')
+        return business
+
+
+    @classmethod
+    def get_business(cls, id):
+        business = cls.objects.get(id=id)
+        return business
+
+    @classmethod
+    def search_by_name(cls,search_term):
+        business=cls.objects.filter(business_name__icontains=search_term)
+        return business
+
+    
+ 
