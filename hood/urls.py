b6750c3c780796b0f2e089fa55d95ac52abe77fe
sylvester omondi <sylvesteromondi49@gmail.com>
2019-01-12 12:30:47 +0300

    created  bussiness  view functions and set their urls

--- hood/urls.py
+++ hood/urls.py
@@ -5,7 +5,11 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     url(r'^$',views.hood,name='hood'),
-    url(r'^post',views.new_neighbour,name='new_neighbour')
+    url(r'^post',views.new_neighbour,name='new_neighbour'),
+    url(r'^business/(?P<pk>\d+)/$',views.new_business,name='new_business'),
+    url(r'^show/(?P<neighbourhoud_id>\d+)/$', views.show, name='show'), 
+    url(r'^search/$',views.search , name='search'),
+    url(r'^search_business/$',views.search , name='search_business'),
 ]
 
 if settings.DEBUG:
--- hood/views.py
+++ hood/views.py
@@ -6,10 +6,11 @@ from .forms import *
 # Create your views here.
 def hood(request):
     current_user= request.user
-    neighbour=Neighbourhood.objects.all()
-    
+    neighbour=Neighbourhood.objects.filter()
+    business =Business.objects.filter(user=request.user.id)
 
-    return render(request, 'index.html')
+
+    return render(request, 'index.html',{"neighbour":neighbour,"current_user":current_user,"bussiness":bussiness})
 
 def new_neighbour(request):
     current_user = request.user
@@ -23,6 +24,46 @@ def new_neighbour(request):
 
     else:
         form = NeighbourhoodForm()
-    return render(request,'hood.html', {"form": form})  
+    return render(request,'hood.html', {"form": form}) 
+
+def show(request, neighbourhood_id):
+    neighbour=Neighbourhood.objects.get(pk = neighbourhood_id)
+    current_user= request.user
+    business=BusinessForm()
+    biz =Business.objects.filter(neighbourhood=neighbourhood_id)
+    context = {"neighbour":neighbour,"current_user":current_user,"business":business,"biz":biz}
+    return render(request, 'show.html', context )
 
 
+
+
+def new_business(request, pk):
+    neighbourhood = get_object_or_404(Neighbourhood,id=pk)
+    current_user = request.user
+    if request.method == 'POST':
+        form = BusinessForm(request.POST, request.FILES)
+        if form.is_valid():
+            business = form.save(commit=False)
+            business.user = current_user
+            print('x')
+            business.neighbourhood = neighbourhood
+            business.save()
+        return redirect('show', neighbourhood_id=neighbourhood.id)
+
+    else:
+        form = BusinessForm()
+    return render(request, 'business.html', {"form": form, "neighbourhood":neighbourhood })
+
+ 
+
+def search(request):
+
+    if 'neighbourhood' in request.GET and request.GET["neighbourhood"]:
+        search_term = request.GET.get("neighbourhood")
+        searched_neighbor = Neighbourhood.search_by_name(search_term)
+        # message=f'{search_term}'
+        return render(request, 'search.html',{"neighbour": searched_neighbor})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})
