495f356d12b18c8045272f29b89a5c57b292c070
sylvester omondi <sylvesteromondi49@gmail.com>
2019-01-14 11:35:06 +0300

    iinputted urls to help in search edit profile,delete project and create project

--- hood/urls.py
+++ hood/urls.py
@@ -6,10 +6,19 @@ from django.conf.urls.static import static
 urlpatterns=[
     url(r'^$',views.hood,name='hood'),
     url(r'^post',views.new_neighbour,name='new_neighbour'),
-    url(r'^business/(?P<pk>\d+)/$',views.new_business,name='new_business'),
-    url(r'^show/(?P<neighbourhoud_id>\d+)/$', views.show, name='show'), 
+    url(r'^business/(?P<pk>\d+)',views.new_business,name='new_business'),
+    url(r'^show/(\d+)', views.show, name='show'), 
     url(r'^search/$',views.search , name='search'),
     url(r'^search_business/$',views.search , name='search_business'),
+    url('edit_profile/',views.edit_profile, name='edit_profile'),
+    url('edit/',views.edit, name='edit'),
+    url( r'^profile/$' , views.profile , name='profile' ),
+    url( r'pro/(?P<pk>[0-9]+)/$' , views.dump, name='dump' ),
+    url( r'project/(?P<pk>[0-9]+)/$' , views.AlbumUpdate.as_view( ) , name='album-update' ) ,
+    url( r'prof/(?P<pk>[0-9]+)/$' , views.ProfileUpdate.as_view( ) , name='profile-update' ) ,
+    url( r'project/(?P<pk>[0-9]+)/delete/$' , views.AlbumDelete.as_view( ) , name='album-delete' ) ,
+    url( r'profile/(?P<pk>[0-9]+)/delete/$' , views.ProfileDelete.as_view( ) , name='profile-delete' ) ,
+    url( r'^create/$' , views.create , name='create' ),
 ]
 
 if settings.DEBUG:
